a69881cecfe97f2d756f6f2776e6041a44450723
Nedim <nedimesken@kreatinmedya.com>
2026-01-28 20:43:42 +0300

    feat: Implement database connection using SQLAlchemy, add a `/db-test` endpoint, and rename the root endpoint.

--- main.py
+++ main.py
@@ -1,7 +1,30 @@
 from fastapi import FastAPI
+import os
+from sqlalchemy import create_engine, text
 
 app = FastAPI()
 
+DB_USER = os.getenv("DATABASE_USER")
+DB_PASS = os.getenv("DATABASE_PASSWORD")
+DB_HOST = os.getenv("DATABASE_HOST")
+DB_PORT = os.getenv("DATABASE_PORT")
+DB_NAME = os.getenv("DATABASE_NAME")
+
+DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+engine = create_engine(DATABASE_URL)
+
 @app.get("/")
-def home():
+def root():
     return {"status": "API is running"}
+
+@app.get("/db-test")
+def db_test():
+    with engine.connect() as conn:
+        result = conn.execute(text("SELECT NOW()"))
+        row = result.fetchone()
+
+        return {
+            "db": "connected",
+            "time": str(row[0])
+        }
